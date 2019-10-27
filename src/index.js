45a9e8ad6985958ac535ef19d40313eca2cbcc6c
Filip Kotenko <39651273+Kotfil@users.noreply.github.com>
2019-10-27 16:15:08 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log ('Hello Filipp');
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+};
+
+
+getResource('https://swapi.co/api/people/1/')
+.then((body) => {
+    console.log(body);
+})
+.catch((err) => {
+    console.error('Cloud not fetch',err);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch('https://swapi.co/api/people/1/')
+// .then((res) => {
+//    return res.json();
+// })
+// .then((body) => {
+//     console.log(body);
+// });
