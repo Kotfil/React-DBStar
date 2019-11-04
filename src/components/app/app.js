e5727b8ab8fba1e1fb11cdd901584a415af66632
Filip <39651273+Kotfil@users.noreply.github.com>
2019-11-04 23:41:02 +0200

    new

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,7 +11,7 @@ export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: 5
+    selectedPerson: null
   };
 
   toggleRandomPlanet = () => {
@@ -40,12 +40,15 @@ export default class App extends Component {
       <div className="stardb-app">
         <Header />
         { planet }
-
+<div className ="row mb2 button-row">
         <button
           className="toggle-planet btn btn-warning btn-lg"
           onClick={this.toggleRandomPlanet}>
           Toggle Random Planet
         </button>
+<ErrorButton/>
+
+        </div>
 
         <div className="row mb2">
           <div className="col-md-6">
